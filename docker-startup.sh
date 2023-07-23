a0541e7e12b62d8c9e9b94f096f61c2c14598ff6
Bengeta <inovozhilov08@gmail.com>
2023-07-23 17:41:13 +1000

    deploy to server

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./bundle --connection "User ID=postgres;Password=miva;Host=${DB_ANALYSIS_HOST};Port=5432;Database=AnalysisDB;Pooling=false; Connection Idle Lifetime=10; Max Auto Prepare=20;"
-dotnet AnalysisService.dll
+./bundle --connection "User ID=postgres;Password=miva;Host=${DB_EVENT_HOST};Port=5432;Database=EventDB;Pooling=false; Connection Idle Lifetime=10; Max Auto Prepare=20;"
+dotnet EventService.dll
